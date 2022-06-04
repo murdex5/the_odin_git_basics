a2ca7e725c08819ab829ab99a41562e7f92a669a
murdex5 <fkadupiige@gmail.com>
2022-06-04 23:17:29 +0200

    Rock paper scissors logic implimented

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,7 +1,45 @@
 function computerPlay() {
-    let words = ["Rock", "Paper", "Scissors"]
+    let words = ["Rock", "Paper", "Scissors"];
     randomNum = Math.floor(Math.random(0, words.length) * words.length);
-    return words[randomNum]
+    return words[randomNum];
 };
 
-console.log(computerPlay())
+function playerPlay(word) {
+    return word;
+};
+
+function playRound(playerSelection, computerSelection) {
+    let ps = playerSelection;
+    let cs = computerSelection;
+    let rock = "Rock";
+    let paper = "Paper";
+    let scissors = "Scissors";
+    if (ps == rock && cs == paper) { // Rock player lose
+        return "You lose! " + cs + " beats " + ps + ".";
+    } else if (ps == rock && cs == scissors) { // Rock player win
+        return "You win! " + ps + " beats " + cs + ".";
+    } else if (ps == rock && cs == ps) { // Rock player same
+        return "Tie... Try again!";
+    } else if (ps == paper && cs == scissors) { // Paper player lose
+        return "You lose! " + cs + " beats " + ps + ".";
+    } else if (ps == paper && cs == rock) { // Paper player win
+        return "You win! " + ps + " beats " + cs + ".";
+    } else if (ps == paper && cs == ps) { // Paper player same
+        return "Tie... Try again!";
+    } else if (ps == scissors && cs == rock) { // Scissors player lose
+        return "You lose! " + cs + " beats " + ps + ".";
+    } else if (ps == scissors && cs == paper) { // Scissors player win
+        return "You win! " + ps + " beats " + cs + ".";
+    } else if ( ps == scissors && cs == ps) { // Scissors player same
+        return "Tie... Try again!";
+    } else {
+        return "Enter the correct value"
+    }
+};
+
+let playerWord = playerPlay("Rock");
+let computerWord = computerPlay();
+
+console.log("You chose " + playerWord)
+console.log("Computer chose " + computerWord)
+console.log(playRound(playerWord, computerWord))
