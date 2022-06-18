3cb6342c88e6cfe5bb8b57ad7568ce8264278dc7
murdex5 <fkadupiige@gmail.com>
2022-06-18 14:55:47 +0200

    Java script file shows number

--- javascript.js
+++ javascript.js
@@ -1,4 +1,9 @@
 const max = 57;
 let actual = max - 13;
 let percentage = actual / max;
-console.log(percentage.toFixed(4))
+console.log(percentage.toFixed(4));
+let myStr = "Hello mage raththaran yaluvane!";
+console.log(myStr.substring(0, 4))
+document.getElementById("js").innerHTML = myStr.slice(7);
+let newTxt = myStr.padEnd(4, "i");
+document.getElementById("replace").innerHTML = newTxt;
